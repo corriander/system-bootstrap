b794a748c42f9fd3a2e7889467fd4a7868eee201
Alex Corrie <ajccode@gmail.com>
2023-08-11 16:10:34 +0100

    feat(build): fix ssh url parsing in build script
    Github user/repo/branch is now correctly pulled from an ssh remote

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 mkdir -p target
-rm target/bootstrap*
+rm target/bootstrap* 2>/dev/null
 bashing uberbash 
 sed -i 's/^ *EOF$/EOF/g' ./target/bootstrap-*.sh 
 cp ./target/bootstrap* ./target/bootstrap.sh 
-REMOTE="$(git remote -v | grep -E 'origin.*fetch' | cut -b 27- | sed 's/\.git .*//')"
-sed -i "s@raw.githubusercontent.com/.*@raw.githubusercontent.com/$REMOTE/master/target/bootstrap.sh@g" README.md
+REMOTE_REPO=$(git remote get-url origin | cut -d: -f2)
+REMOTE="${REMOTE_REPO/.git/}/$(git rev-parse --abbrev-ref HEAD)"
+sed -i "s@raw.githubusercontent.com/.*@raw.githubusercontent.com/$REMOTE/target/bootstrap.sh@g" README.md
