146d9dd23c762cbe3280d4a31825e8a49de5ef08
Alex Corrie <ajccode@gmail.com>
2023-08-13 12:51:47 +0100

    fix: add to and reorganise base packages
    python3-venv is needed by Mason to install the python ls
    wslu is no longer pre-installed in ubuntu 22.04 and needs to be added
    via ppa

--- src/lib/defaults.sh
+++ src/lib/defaults.sh
@@ -5,12 +5,23 @@ function install_base_packages() {
     # NOTE: Update this with the latest tool added to the list
     sudo apt update
 
-    sudo apt install -y keychain gcc g++ make cmake fd-find ripgrep build-essential jq unzip
+    echo "Installing base packages"
+    sudo apt install -y keychain build-essential jq unzip
+
+    echo "Installing WSL utilities"
+    sudo add-apt-repository -y ppa:wslutilities/wslu
+    sudo apt update
+    sudo apt install -y wslu
+
+    echo "Installing neovim dependencies"
+    # pythong3-venv is needed for Mason to install python lsp
+    sudo apt install -y gcc g++ make cmake fd-find ripgrep python3-venv
 
     echo "Installing pyenv build environment dependencies"
     # Required to successfully build python
     # See https://github.com/pyenv/pyenv/wiki#suggested-build-environment
     sudo apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+    #
     # Couple of additional ones sourced elsewhere
     sudo apt install -y zlib1g lzma
 }
